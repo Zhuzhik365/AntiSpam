09cb732463f48f335a0e6d8c38e3bd8f5bf0f57c
Zhuhik365 <andalemerzliakov@edu.hse.ru>
2025-05-30 21:55:18 +0300

    application

---
+++ alg.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import numpy as np
+import re
+
+# Загрузка датасета
+df = pd.read_csv('SMSSpamCollection', sep='\t', header=None, names=['label', 'message'])
+
+
+# Функция предобработки текста
+def preprocess_text(text):
+    text = text.lower()  # Приводим к нижнему регистру
+    text = re.sub(r'[^a-z\s]', '', text)  # Удаляем все кроме букв и пробелов
+    text = re.sub(r'\s+', ' ', text).strip()  # Удаляем лишние пробелы
+    return text
+
+
+# Применяем предобработку к сообщениям
+df['clean_message'] = df['message'].apply(preprocess_text)
+
+# Разделение на спам и не-спам
+spam_df = df[df['label'] == 'spam']
+ham_df = df[df['label'] == 'ham']
+
+spam_df.to_csv("spam.csv")
+
+
+# Создание словарей частот слов
+def create_freq_dict(messages):
+    freq_dict = {}
+    for message in messages:
+        for word in message.split():
+            freq_dict[word] = freq_dict.get(word, 0) + 1
+    return freq_dict
+
+
+# Создаем частотные словари
+spam_freq = create_freq_dict(spam_df['clean_message'])
+ham_freq = create_freq_dict(ham_df['clean_message'])
+
+# Расчет спам-индикаторов для слов
+word_spam_scores = {}
+total_spam = len(spam_df)
+total_ham = len(ham_df)
+
+for word in set(spam_freq.keys()).union(ham_freq.keys()):
+    spam_count = spam_freq.get(word, 0) + 1  # Добавляем 1 для сглаживания
+    ham_count = ham_freq.get(word, 0) + 1
+
+    # Рассчитываем вероятность слова в спаме
+    spam_prob = spam_count / total_spam
+    ham_prob = ham_count / total_ham
+
+    # Вычисляем спам-индикатор
+    word_spam_scores[word] = spam_prob / (spam_prob + ham_prob)
+
+def spam_words(text):
+    # Очищаем и разбиваем текст на слова
+    clean_text = preprocess_text(text)
+    words = clean_text.split()
+
+    if not words:
+        return False, 0.0  # Пустые сообщения не спам
+
+    # Собираем индикаторы для слов из сообщения
+    scores = []
+    triger_wrods = []
+    for word in words:
+        if word in word_spam_scores:
+            scores.append(word_spam_scores[word])
+            triger_wrods.append([word,word_spam_scores[word]])
+
+    return triger_wrods
+
+
+# Функция проверки сообщения на спам
+def check_spam(text, threshold=0.6):
+    # Очищаем и разбиваем текст на слова
+    clean_text = preprocess_text(text)
+    words = clean_text.split()
+
+    if not words:
+        return False, 0.0  # Пустые сообщения не спам
+
+    # Собираем индикаторы для слов из сообщения
+    scores = []
+    for word in words:
+        if word in word_spam_scores:
+            scores.append(word_spam_scores[word])
+
+    # Если нет известных слов, считаем не спамом
+    if not scores:
+        return False, 0.0
+
+    # Усредняем индикаторы
+    spam_score = np.mean(scores)
+
+    return spam_score > threshold, spam_score
+
+spam_words(" spam")
---
+++ interface.py
@@ -0,0 +1,148 @@
+import tkinter as tk
+import datetime
+from CTkTable import *
+from PIL import Image
+from customtkinter import *
+
+import alg
+
+
+class App(CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        bg_color = self._apply_appearance_mode(self.cget("background"))
+        print(bg_color)
+
+        name = CTkLabel(self, text="Проверка сообщения на спам", font=("Arial", 20, "bold"))
+        name.pack(padx=10, pady=10, anchor="n")
+
+        # Основной фрейм
+        main_frame = CTkFrame(self, fg_color=bg_color)
+        main_frame.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
+
+        # Фрейм для вводо сообщения
+        vvod_frame = CTkFrame(main_frame, fg_color=bg_color, border_color="gray", border_width=2, corner_radius=10, height=500)
+        vvod_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+        l = CTkLabel(vvod_frame, text="Введите сообщение для проверки")
+        l.grid(row=0, column=0, padx=5, pady=5)
+        l2=CTkLabel(vvod_frame, text="Cообщение должно быть на английском\nВ файле, одна строка - одно сообщение", text_color='gray', font=("Arial", 10), justify="left")
+        l2.grid(row=1)
+
+        input_text = CTkTextbox(vvod_frame)
+        input_text.grid(row=2, padx=10, sticky="nsew")
+
+        def rezume(val):
+            rez = CTkToplevel()
+            rez.geometry("400x400")
+            rez.title("Отчет по проверке")
+
+
+
+            s = CTkScrollableFrame(rez, width=400, height=400)
+            s.pack(expand=True, anchor='center')
+
+            l = CTkLabel(s, text='Процент спам слов получен \nиз анализа файла SMSSpamCollection')
+            l.pack()
+
+            t = CTkTable(s, values=val)
+            t.pack(expand=True, padx=10, pady=10)
+
+        # Проверка сообщения и вывод результата
+
+        def print_text():
+            text = input_text.get("1.0", "end-1c")
+            is_spam, score = alg.check_spam(text)
+            if is_spam:
+                ans_text.delete(0.0, 'end')
+                ans_text.configure(border_color="red")
+                ans_text.insert(END, f"Сообщение: {text}\n\n"+"Вероятность: " + str(int(score*100))+"%")
+            else:
+                ans_text.delete(0.0, 'end')
+                ans_text.configure(border_color="green")
+                ans_text.insert(END, f"Сообщение: {text}\n\n"+"Вероятность: " + str(int(score*100))+"%")
+            val = [['Слово', "% появления в спаме"]]
+            v = alg.spam_words(text)
+            for i in v:
+                val.append(i)
+            rezume(val)
+
+
+
+        # Кнопка ввода
+        btn_frame = CTkFrame(vvod_frame)
+        btn_frame.grid(row=3, padx=10, pady=10, sticky="nsew")
+
+        btn = CTkButton(btn_frame, text="Ввод", command=print_text, width=135, height=35)
+        btn.grid(row=0, column=1, padx=5, pady=5)
+
+
+        # Загрузка текста из файла
+        def load_from_file():
+            file_path = filedialog.askopenfilename(title="Выберите файл", filetypes=[('Text Files', '*.txt'), ('csv','*.csv')])
+            file_name = file_path[file_path.rfind("/")+1:]
+            with open(file_path, "r", encoding="utf-8") as file:
+                text = file.readlines()
+                ans_text.delete(0.0, 'end')
+                input_text.delete(0.0, 'end')
+                input_text.insert(END, "Файл: "+file_name)
+                val = [['Слово', "% появления в спаме"]]
+                for line in text:
+                    spam, score= alg.check_spam(line)
+                    ans_text.insert(END, f"Сообщение: {line}")
+                    ans_text.insert(END, f"Вероятность спама: {str(int(score*100))}%\n\n")
+                    v = alg.spam_words(line)
+                    for i in v:
+                        val.append(i)
+                rezume(val)
+
+
+
+
+        def save_text():
+            """Сохраняет содержимое текстового поля в файл"""
+            # Получаем текст из виджета
+            text_content = ans_text.get("1.0", "end-1c")
+            text_content = "Дата записи: "+str(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))+"\n"+text_content
+
+            # Открываем диалоговое окно сохранения
+            file_path = filedialog.asksaveasfilename(
+                defaultextension=".txt",
+                filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")],
+                title="Сохранить текст как"
+            )
+
+            # Если пользователь не отменил диалог
+            if file_path:
+                try:
+                    # Записываем текст в файл
+                    with open(file_path, "w", encoding="utf-8") as file:
+                        file.write(text_content)
+                except Exception as e:
+                    print(e)
+
+        # Кнопка выбора файла
+        icon = CTkImage(Image.open("../AntiSpam/icn1.png"))
+        btn_file = CTkButton(btn_frame, width=35, image=icon, text='', command=load_from_file)
+        btn_file.grid(row=0, column=0, padx=5, pady=5)
+
+        # Фрейм с ответом
+        ans_frame = CTkFrame(main_frame)
+        ans_frame.grid(row=1, column=1, padx=10, pady=10)
+
+        ans_text = CTkTextbox(main_frame, border_width=2, border_color="gray", corner_radius=10, width=400)
+        ans_text.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+
+        btnFrame2 = CTkFrame(main_frame, fg_color="transparent")
+        btnFrame2.grid(row=2, column=1)
+
+        save_btn = CTkButton(btnFrame2, text="Сохранить", command=save_text)
+        save_btn.grid(row=0, column=0, padx=10)
+
+
+
+app = App()
+app.resizable(False, False)
+app.title("AntiSpam")
+app.mainloop()
